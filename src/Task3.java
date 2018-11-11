5fa7df23d9dd6ecc6ec16e09f2e476faa1d5b804
TamaraStankovska <stankovskatamara23@yahoo.com>
2018-11-12 00:31:32 +0100

    Solved Task 3

--- src/Task3.java
+++ src/Task3.java
@@ -3,16 +3,62 @@ import static java.lang.System.*;
 
 import domain.Task;
 import domain.TaskType;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task3 {
 
     public static void main(String[] args) {
         List<Task> tasks = getTasks();
-        Map<TaskType, List<Task>> allTasksByType = new HashMap<>();
+
+
+        System.out.println("----Task with longest title----");
+        System.out.println(
+                tasks.stream()
+                        .map(Task::getTitle)
+                        // .mapToInt(String::length)
+                        .max(Comparator.comparing(String::length))
+        );
+
+
+        System.out.println("\n");
+        System.out.println("----Total number of tags----");
+        System.out.println(
+                tasks.stream()
+                        .flatMap(task -> task.getTags().stream())
+                        .count());
+
+
+
+        System.out.println("\n");
+        System.out.println("---Summary of Task titles---");
+        tasks.stream()
+                .map(Task::getTitle)
+                .forEach(System.out::println);
+
+
+        System.out.println("\n");
+        System.out.println("---Grouping Tasks by Type---");
+        System.out.println(
+                tasks.stream()
+                        .collect(Collectors.groupingBy(Task::getType)));
+
+
+        System.out.println("\n");
+        System.out.println("---Grouping Tasks by Type and Date Created On---");
+
+        Map<TaskType, Map<LocalDate, List<Task>>> groupedTasksbyTypeAndDate = tasks.stream()
+                .collect(Collectors.groupingBy(Task::getType, Collectors.groupingBy(Task::getCreatedOn)));
+
+        groupedTasksbyTypeAndDate.entrySet()
+                .forEach(System.out::println);
+
+
+
+
+       /* Map<TaskType, List<Task>> allTasksByType = new HashMap<>();
         for (Task task : tasks) {
             List<Task> existingTasksByType = allTasksByType.get(task.getType());
             if (existingTasksByType == null) {
@@ -26,5 +72,7 @@ public class Task3 {
         for (Map.Entry<TaskType, List<Task>> entry : allTasksByType.entrySet()) {
             out.println(String.format("%s =>> %s", entry.getKey(), entry.getValue()));
         }
+    }*/
     }
 }
+
